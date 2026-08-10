c3f95e6031cd24e71b0dde5e99746cb22a628a7c
fizzaabbas-dev <fizzaabbas.398@gmail.com>
2026-08-10 02:59:52 -0700

    connect monogodb

--- index.js
+++ index.js
@@ -1,20 +1,23 @@
-import cors from 'cors';
 import express from "express";
+import cors from "cors";
 import dotenv from "dotenv";
-import { ConnectMongoDb } from './Utils/mongodb.js';
+import { ConnectMongoDb } from "./Utils/mongodb.js";
 import URLRoute from "./Routes/urls.js";
+import dns from "dns/promises"
 
+dns.setServers(["1.1.1.1","8.8.8.8"])
 
 dotenv.config();
 
-import dns from "node:dns/promises";
-dns.setServers(["1.1.1.1","8.8.8.8"])
-
 const app = express();
+
 app.use(cors());
 app.use(express.json());
 
+ConnectMongoDb();
+
+app.use("/", URLRoute);
 
 app.listen(5050, () => {
-  console.log("Server is running on port 5050");
+  console.log("I am working on PORT 5050");
 });
